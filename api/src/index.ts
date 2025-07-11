80c5cf82f63e18269029b79f52ded66feae49e4c
Stipo Margic <stipo@udruga-liberato.hr>
2025-07-11 20:05:47 +0200

    .

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,44 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cityRoutes from './routes/city-routes';
-import locationRoutes from './routes/location-routes';
-import categoryRoutes from './routes/category-routes';
-import userRoutes from './routes/user-routes';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import volunteerRoutes from './routes/volunteer-routes';
-import taskRoutes from './routes/task-routes';
-import emailRoutes from './routes/email-routes';
-import questionRoutes from './routes/question-routes';
-import answerRoutes from './routes/answer-routes';
-import memberRoutes from './routes/member-routes';
 import { verifyWebhook } from '@clerk/express/webhooks';
-import imageRoutes from './routes/image-routes';
-import imageLocationRoutes from './routes/image-location-routes';
-import authRoutes from './routes/auth-routes';
-
-dotenv.config();
+import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
-
-app.get('/', (request, res) => {
-  res.send('Hello World!');
-});
 
 app.post(
   '/api/webhooks',
   express.raw({ type: 'application/json' }),
   async (req, res) => {
-    console.log('Received webhook request:', req.body);
     try {
-      const evt = await verifyWebhook(req, {
-        signingSecret: process.env.CLERK_WEBHOOK_SIGNING_SECRET,
-      });
+      const evt = await verifyWebhook(req);
 
       // Do something with payload
       // For this guide, log payload to console
@@ -50,30 +20,14 @@ app.post(
       console.log('Webhook payload:', evt.data);
 
       res.send('Webhook received');
-    } catch (error) {
-      console.error('Error verifying webhook:', error);
+    } catch (err) {
+      console.error('Error verifying webhook:', err);
       res.status(400).send('Error verifying webhook');
     }
   },
 );
-
-app.use('/api/auth', authRoutes);
-
-app.use('/api/cities', cityRoutes);
-app.use('/api/locations', locationRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/volunteers', volunteerRoutes);
-app.use('/api/tasks', taskRoutes);
-app.use('/api/emails', emailRoutes);
-app.use('/api/questions', questionRoutes);
-app.use('/api/answers', answerRoutes);
-app.use('/api/members', memberRoutes);
-app.use('/api/images', imageRoutes);
-app.use('/api/image_locations', imageLocationRoutes);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log(`Server is running on 3000 `);
 });
 
 export { app };
